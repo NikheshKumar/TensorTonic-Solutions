c2c1d47f1a64608ec321b5ed5761bdc244dc970a
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-02-05 20:21:56 +0530

    Add solution for perplexity-computation

---
+++ perplexity-computation/perplexity-computation.py
@@ -0,0 +1,23 @@
+def perplexity(prob_distributions, actual_tokens):
+    """
+    Compute the perplexity of a token sequence given predicted distributions.
+    """
+    # Write code here
+    import numpy as np 
+
+    prob_distributions = np.asarray(prob_distributions, float)
+    actual_tokens = np.asarray(actual_tokens, int)
+
+    n = len(actual_tokens)
+    if n == 0:
+        return None
+
+    p = prob_distributions[np.arange(n), actual_tokens]
+    
+    p = np.clip(p, 1e-12, 1.0)
+
+    H = - np.mean(np.log(p))
+
+    PP = np.exp(H)
+
+    return PP
