7735a1c4e3ce991f257a485676a5257add2c7cc6
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-02-14 00:47:40 +0530

    Add solution for double-exponential-smoothing

---
+++ double-exponential-smoothing/double-exponential-smoothing.py
@@ -0,0 +1,20 @@
+def double_exponential_smoothing(series, alpha, beta):
+    """
+    Apply Holt's linear trend method and return the level values.
+    """
+    # Write code here
+    import numpy as np 
+
+    series = np.asarray(series)
+
+    l = np.zeros(len(series))
+    b = np.zeros(len(series))
+
+    b[0] = series[1] - series[0]
+    l[0] = series[0]
+
+    for t in range(1, len(series)):
+        l[t] = alpha*series[t] + (1-alpha)*( l[t-1] + b[t-1])
+        b[t] = beta*(l[t] - l[t-1]) + (1-beta)*(b[t-1])
+
+    return l.tolist()
