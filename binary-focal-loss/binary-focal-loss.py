8812b29eea8f32d6675c9460ccdb8d154b3dad35
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-02-13 20:10:47 +0530

    Add solution for binary-focal-loss

---
+++ binary-focal-loss/binary-focal-loss.py
@@ -0,0 +1,16 @@
+def binary_focal_loss(predictions, targets, alpha, gamma):
+    """
+    Compute the mean binary focal loss.
+    """
+    # Write code here
+
+    import numpy as np 
+
+    predictions = np.asarray(predictions)
+    targets = np.asarray(targets)
+
+    pt = np.where(targets==1, predictions, 1-predictions)
+
+    fl = -(alpha)*( (1-pt)**gamma )*np.log(pt)
+
+    return np.mean(fl)
