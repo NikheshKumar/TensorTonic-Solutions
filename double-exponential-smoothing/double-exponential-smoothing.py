b002147f7152e24b49317d06f03115d84d606d59
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-02-14 00:50:12 +0530

    Update solution for double-exponential-smoothing

--- double-exponential-smoothing/double-exponential-smoothing.py
+++ double-exponential-smoothing/double-exponential-smoothing.py
@@ -8,13 +8,12 @@ def double_exponential_smoothing(series, alpha, beta):
     series = np.asarray(series)
 
     l = np.zeros(len(series))
-    b = np.zeros(len(series))
 
-    b[0] = series[1] - series[0]
+    b = series[1] - series[0]
     l[0] = series[0]
 
     for t in range(1, len(series)):
-        l[t] = alpha*series[t] + (1-alpha)*( l[t-1] + b[t-1])
-        b[t] = beta*(l[t] - l[t-1]) + (1-beta)*(b[t-1])
+        l[t] = alpha*series[t] + (1-alpha)*( l[t-1] + b)
+        b = beta*(l[t] - l[t-1]) + (1-beta)*(b)
 
     return l.tolist()
