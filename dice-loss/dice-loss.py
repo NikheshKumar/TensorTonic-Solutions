667fb047879a095dc6f0c8152bada51c5623d571
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-02-13 19:48:22 +0530

    Add solution for dice-loss

---
+++ dice-loss/dice-loss.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def dice_loss(p, y, eps=1e-8):
+    """
+    Compute Dice Loss for segmentation.
+    """
+    # Write code here
+    p = np.asarray(p)
+    y = np.asarray(y)
+
+    num = 2 * np.sum(p*y) + eps
+
+    den = np.sum(p) + np.sum(y) + eps
+
+    return 1 - (num / den)
