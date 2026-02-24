1535cf2b6f7dab1e12b6a849fb22d14a5c615b2e
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-02-24 12:46:47 +0530

    Update solution for novelty-score

--- novelty-score/novelty-score.py
+++ novelty-score/novelty-score.py
@@ -5,6 +5,9 @@ def novelty_score(recommendations, item_counts, n_users):
     # Write code here
     import numpy as np 
 
+    if len(recommendations)==0:
+      return 0.0
+
     recommendations = np.asarray(recommendations)
     item_counts = np.asarray(item_counts)
     N = len(recommendations)
