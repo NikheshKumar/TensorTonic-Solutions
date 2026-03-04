cc0917f13d99ef076ba58531c7e9d7bda64d2a65
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-03-04 13:09:30 +0530

    Update solution for batch-normalization

--- batch-normalization/batch-normalization.py
+++ batch-normalization/batch-normalization.py
@@ -5,23 +5,26 @@ def batch_norm_forward(x, gamma, beta, eps=1e-5):
     Forward-only BatchNorm for (N,D) or (N,C,H,W).
     """
     # Write code here
-    x = np.asarray(x, dtype=float)
-    gamma = np.asarray(gamma, dtype=float)
-    beta = np.asarray(beta, dtype=float)
+    x = np.asarray(x, float)
+    gamma = np.asarray(gamma, float)
+    beta = np.asarray(beta, float)
 
     if x.ndim == 2:
-        mu = np.mean(x, axis=0, keepdims=True)
-        var = np.mean((x - mu) ** 2, axis=0, keepdims=True)
+      m = np.mean(x, axis=0, keepdims=True)
+      var = np.mean( (x-m)**2, axis=0, keepdims=True  )
 
     elif x.ndim == 4:
-        mu = np.mean(x, axis=(0,2,3), keepdims=True)
-        var = np.mean( (x-mu)**2, axis=(0,2,3), keepdims=True) 
+      m = np.mean(x, axis=(0,2,3), keepdims=True)
+      var = np.mean( (x-m)**2 , axis=(0,2,3), keepdims=True )
 
-        gamma = gamma.reshape(1, -1, 1, 1)
-        beta = beta.reshape(1, -1, 1, 1)
+      gamma = gamma.reshape(1,-1,1,1)
+      beta = beta.reshape(1,-1,1,1)
 
-    new_x = (x-mu) / (np.sqrt(var + eps))
-
-    y = gamma*new_x + beta
+    x_new = (x-m) / np.sqrt(var + eps)
+    y = gamma * x_new + beta
 
     return y
+      
+
+    
+  
