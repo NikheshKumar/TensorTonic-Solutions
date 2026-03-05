0ad9473f62fb0a1c933ac2bcc314a022b34f6c71
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-03-05 10:16:15 +0530

    Update solution for dropout-training

--- dropout-training/dropout-training.py
+++ dropout-training/dropout-training.py
@@ -6,24 +6,21 @@ def dropout(x, p=0.5, rng=None):
     Return (output, dropout_pattern).
     """
     # Write code here
-    
-    x = np.asarray(x)
-
+    x = np.asarray(x, float)
+  
     if rng is None:
-        rng = np.random 
+      rng = np.random
 
-    random_values = rng.random(x.shape)
+    random_vals = rng.random(x.shape)
+    mask = random_vals/(1-p) < 1
 
-    mask = random_values < (1 - p)    
-
-    if p==0.0:
-        dropout_pattern = np.ones_like(x)
     if p==1.0:
-        dropout_pattern = np.zeros_like(x)    
+      dropout_pattern = np.zeros_like(x)
+    if p==0.0:
+      dropout_pattern = np.ones_like(x)
     else:
-        dropout_pattern = mask.astype(float) / (1 - p)    
-
+      dropout_pattern = mask.astype(float) /(1-p)
 
     output = x * dropout_pattern
 
-    return (output, dropout_pattern)
+    return output, dropout_pattern
