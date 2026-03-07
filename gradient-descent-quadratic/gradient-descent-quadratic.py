f607badd65c58f7d09071244d27244a83b6dc8d0
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-03-07 16:55:46 +0530

    Update solution for gradient-descent-quadratic

--- gradient-descent-quadratic/gradient-descent-quadratic.py
+++ gradient-descent-quadratic/gradient-descent-quadratic.py
@@ -3,11 +3,12 @@ def gradient_descent_quadratic(a, b, c, x0, lr, steps):
     Return final x after 'steps' iterations.
     """
     # Write code here
-    def df(a,b,x):
-        return 2*a*x + b
+    import numpy as np 
 
     x = x0
-    for i in range(steps):
-        x = x - lr * df(a,b,x)
 
-    return x    
+    for i in range(steps):
+      grad = 2*a*x + b
+      x = x - lr * grad
+
+    return float(x)
