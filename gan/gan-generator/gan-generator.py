8d1973ef64fba3318a148dc6f1a28ec1f02f92e9
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-03-10 12:27:07 +0530

    Update solution for gan-generator

--- gan/gan-generator/gan-generator.py
+++ gan/gan-generator/gan-generator.py
@@ -9,22 +9,22 @@ def generator(z: np.ndarray, output_dim: int) -> np.ndarray:
     z = np.asarray(z, float)
     eps = 1e-3
 
-    def leaky_relu(x, alpha=1e-3):
+    def leaky_relu(x, alpha=0.2):
       return np.maximum(alpha * x, x)
   
     # linear + leaky relu 
-    W1 = np.random.uniform(-eps, eps, (z.shape[1], output_dim))
-    b1 = np.random.uniform(-eps, eps, (output_dim))
-    h1 = leaky_relu(z @ W1 + b1, alpha=1e-3)
+    W1 = np.random.uniform(-eps, eps, size=(z.shape[1], z.shape[1]))
+    b1 = np.random.uniform(-eps, eps, size=(z.shape[1],))
+    h1 = leaky_relu(z @ W1 + b1)
 
     # linear + leaky relu
-    W2 = np.random.uniform(-eps, eps, (h1.shape[1], output_dim))
-    b2 = np.random.uniform(-eps, eps, (output_dim))
-    h2 = leaky_relu(h1 @ W2 + b2, alpha=1e-3)
+    W2 = np.random.uniform(-eps, eps, size=(h1.shape[1], h1.shape[1]))
+    b2 = np.random.uniform(-eps, eps, size=(h1.shape[1],))
+    h2 = leaky_relu(h1 @ W2 + b2)
 
     # linear + tanh
-    W3 = np.random.uniform(-eps, eps, (h2.shape[1], output_dim))
-    b3 = np.random.uniform(-eps, eps, (output_dim))
+    W3 = np.random.uniform(-eps, eps, size=(h2.shape[1], output_dim))
+    b3 = np.random.uniform(-eps, eps, size=(output_dim,))
     x_hat = np.tanh(h2 @ W3 + b3)
 
     # fake image
