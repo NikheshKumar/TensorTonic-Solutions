fad1601202dcc562c1155c5c4323e5697a9d0f0d
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-03-12 09:32:03 +0530

    Update solution for k-means-centroid-update

--- k-means-centroid-update/k-means-centroid-update.py
+++ k-means-centroid-update/k-means-centroid-update.py
@@ -4,21 +4,18 @@ def k_means_centroid_update(points, assignments, k):
     """
     # Write code here
     import numpy as np 
+
     points = np.asarray(points)
     assignments = np.asarray(assignments)
 
-    cen = np.zeros((k, points.shape[1]))
-    count = np.zeros(k, float)
+    centroid = np.zeros((k,points.shape[1]))
 
-    for point, ass in zip(points, assignments):
-      cen[ass] += point
-      count[ass] += 1
+    count = np.zeros(k)
 
-    res = np.divide(cen, count[:, None], out=np.zeros_like(cen), where=count[:, None] != 0)
-  
-    return res.tolist()
+    for p, a in zip(points, assignments):
+      centroid[a] += p
+      count[a] += 1
 
-      
-      
-      
-    
+    ans = np.divide(centroid, count[:,None], out=np.zeros_like(centroid), where=count[:, None] != 0 )
+
+    return ans.tolist()
