4f96db18fdb8875b643da7ac718ec88a92ddbedd
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-03-14 01:17:02 +0530

    Add solution for pca-projection

---
+++ pca-projection/pca-projection.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def pca_projection(X, k):
+    """
+    Project data onto the top-k principal components.
+    """
+    # Write code here
+    X = np.asarray(X, float)
+    N, D = X.shape
+
+    X_c = X - np.mean(X, axis=0)
+
+    cov = X_c.T @ X_c / (N-1)
+
+    eig_vals, eig_vectors = np.linalg.eig(cov)
+
+    index = np.argsort(eig_vals)[::-1][:k]
+
+    W = eig_vectors[:,index]
+
+    proj = X_c @ W
+
+    return proj
