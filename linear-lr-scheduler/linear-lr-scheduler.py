f9dbc4b34c6e769de9847d330aa2741fd3d0ff4a
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-03-14 19:28:10 +0530

    Update solution for linear-lr-scheduler

--- linear-lr-scheduler/linear-lr-scheduler.py
+++ linear-lr-scheduler/linear-lr-scheduler.py
@@ -4,19 +4,21 @@ def linear_lr(step, total_steps, initial_lr, final_lr=0.0, warmup_steps=0) -> fl
     Steps are 0-based; clamp at final_lr after total_steps.
     """
     # Write code here
+    import numpy as np 
+
     lr = float(initial_lr)
 
     if step >= total_steps:
-            lr = float(final_lr)
-            return lr 
+      lr = final_lr
+      return lr
 
     if warmup_steps > 0 and step < warmup_steps:
-            lr = float(step*final_lr/warmup_steps)
-            return lr
-
+      t = step
+      lr = t * initial_lr / warmup_steps
+      
     if warmup_steps <= step <= total_steps:
-            lr = final_lr + (initial_lr - final_lr) * ( (total_steps - step )/(total_steps - warmup_steps ) )
-            lr = float(lr)
-            return lr
+      t = step
+      lr = final_lr + (initial_lr-final_lr) * (total_steps - t) / (total_steps - warmup_steps)
+      
 
-         
+    return float(lr)
