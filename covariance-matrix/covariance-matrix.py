f2598ff2f41f0025cb6ed6c5aab192d88df9ff67
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-03-16 22:31:33 +0530

    Update solution for covariance-matrix

--- covariance-matrix/covariance-matrix.py
+++ covariance-matrix/covariance-matrix.py
@@ -5,24 +5,26 @@ def covariance_matrix(X):
     Compute covariance matrix from dataset X.
     """
     # Write code here
-    X = np.asarray(X)
+    X = np.asarray(X, float)
 
     if X.ndim !=2:
         return None 
 
-    N, D = X.shape
-    
     if X.size == 0:
         return None
+
+    N, D = X.shape
 
     if N < 2:
         return None
 
     if D == 0:
-        return np.zeros((0, 0))    
+        return np.zeros((0, 0)) 
+      
+    mu = np.mean(X, axis=0)
 
-    mu = np.mean(X,axis=0)
-    X_cent = X - mu
+    X_c = X - mu
 
-    cov_mat = (X_cent.T @ X_cent) / (N - 1)
-    return cov_mat
+    cov = (X_c.T @ X_c) / (N-1)
+
+    return cov
