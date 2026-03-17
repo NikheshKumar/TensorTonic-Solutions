c4fe6f043ec091c51373febe5f78278deb45db4d
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-03-17 09:05:20 +0530

    Update solution for nadam-optimizer

--- nadam-optimizer/nadam-optimizer.py
+++ nadam-optimizer/nadam-optimizer.py
@@ -5,10 +5,19 @@ def nadam_step(w, m, v, grad, lr=0.002, beta1=0.9, beta2=0.999, eps=1e-8):
     Perform one Nadam update step.
     """
     # Write code here
-    new_w,new_m,new_v,grad = np.asarray(w), np.asarray(m), np.asarray(v), np.asarray(grad)
+    w = np.asarray(w, float)
+    m = np.asarray(m ,float)
+    v = np.asarray(v, float)
+    grad = np.asarray(grad, float)
 
-    new_m = beta1 * new_m + (1 - beta1) * grad
-    new_v = beta2 * new_v + (1-beta2) * (grad**2)
-    new_w = new_w - lr * (beta1 * new_m + (1-beta1)*grad ) / ( np.sqrt(new_v) + eps)
 
-    return new_w,new_m,new_v
+    m = beta1 * m + (1-beta1) * grad
+    v = beta2 * v + (1-beta2) * (grad**2)
+
+    num = beta1 * m + (1-beta1) * grad
+    den = np.sqrt(v) + eps
+
+    w = w - lr * num / den
+
+    return (w, m, v)
+    
