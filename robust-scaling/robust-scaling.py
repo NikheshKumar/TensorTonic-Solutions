73b3431dc02ee7677e8a315a9cfe0114b560dc3f
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-03-21 14:56:39 +0530

    Update solution for robust-scaling

--- robust-scaling/robust-scaling.py
+++ robust-scaling/robust-scaling.py
@@ -2,35 +2,29 @@ def robust_scaling(values):
     """
     Scale values using median and interquartile range.
     """
+    # Write code here
     import numpy as np 
 
     values = np.asarray(values, float)
+
     values_sorted = np.sort(values)
     n = len(values)
 
-    def compute_median(arr):
+    def calculate_median(arr):
       n = len(arr)
       if n==0:
-        return 0.0  
+        return 0.0
       if n%2==0:
-        med = (arr[(n//2)] + arr[(n//2)-1]) / 2.0
+        median = (arr[n//2] + arr[n//2 - 1]) / 2.0
       else:
-        med = arr[(n//2)]
-      return med
+        median = arr[n//2]
+      return median
 
-    if n % 2 == 1:
-        q1 = compute_median(values_sorted[:n//2])
-        q3 = compute_median(values_sorted[n//2 + 1:])
-    else:
-        q1 = compute_median(values_sorted[:n//2])
-        q3 = compute_median(values_sorted[n//2:])
-      
-    med = compute_median(values_sorted)
-    
-    if q3-q1 == 0:
-      return (values-med).tolist()
+  
+    q1 = calculate_median(values_sorted[:n//2])
+    q3 = calculate_median(values_sorted[n//2:]) if n%2==0 else calculate_median(values_sorted[n//2 + 1:])
+    med = calculate_median(values_sorted)
 
-    val_scaled = ( values - med ) / (q3 - q1)
+    x_scaled = (values - med) / (q3 - q1) if q3-q1!=0 else (values - med)
 
-    return val_scaled.tolist()
-    
+    return x_scaled.tolist()
