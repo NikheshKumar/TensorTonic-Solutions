7cebb5fdcfbd6c3f1bff7c20d88037fb7977eba7
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-03-22 22:12:44 +0530

    Update solution for adam-optimizer

--- adam-optimizer/adam-optimizer.py
+++ adam-optimizer/adam-optimizer.py
@@ -6,17 +6,20 @@ def adam_step(param, grad, m, v, t, lr=1e-3, beta1=0.9, beta2=0.999, eps=1e-8):
     Return (param_new, m_new, v_new).
     """
     # Write code here
-    m = np.asarray(m)
-    v = np.asarray(v)
-    grad = np.asarray(grad)
-    param = np.asarray(param)
-  
-    new_m = beta1 * m + (1-beta1) * grad
-    new_v = beta2 * v + (1-beta2) * (grad**2)
+    param = np.asarray(param, float)
+    grad = np.asarray(grad, float)
+    m = np.asarray(m, float)
+    v = np.asarray(v, float)
 
-    m_hat = new_m / (1 - beta1**t)
-    v_hat = new_v / (1 - beta2**t)
+    m_new = beta1 * m + (1-beta1) * grad
 
-    param_new = param - lr * m_hat / (np.sqrt(v_hat) + eps)
+    v_new = beta2 * v + (1-beta2) * (grad**2)
 
-    return (param_new, new_m, new_v)
+    m_hat = m_new/ (1-beta1**t)
+
+    v_hat = v_new / (1-beta2**t)
+
+    param_new =  param - lr * m_hat / (np.sqrt(v_hat) + eps)
+
+
+    return param_new, m_new, v_new
