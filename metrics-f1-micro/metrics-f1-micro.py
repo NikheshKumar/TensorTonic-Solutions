792183a77206a7622e6ecf40d503c68ddaccd8c7
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-03-23 09:19:11 +0530

    Update solution for metrics-f1-micro

--- metrics-f1-micro/metrics-f1-micro.py
+++ metrics-f1-micro/metrics-f1-micro.py
@@ -3,21 +3,20 @@ def f1_micro(y_true, y_pred) -> float:
     Compute micro-averaged F1 for multi-class integer labels.
     """
     # Write code here
-    import numpy as np
-
-    y_true = np.asarray(y_true, float)
-    y_pred = np.asarray(y_pred, float)
+    import numpy as np 
+    y_true = np.asarray(y_true)
+    y_pred = np.asarray(y_pred)
 
     classes = np.unique(np.concatenate([y_true, y_pred]))
 
-    TP = FP = FN = 0.0
-
-    for cla in classes:
-        TP += np.sum((y_true == cla) & (y_pred == cla))
-        FP += np.sum((y_true != cla) & (y_pred == cla))
-        FN += np.sum((y_true == cla) & (y_pred != cla))
-
-    ans = 2 * TP / (2*TP + FP + FN)
+    tp = fp = fn = 0.0
+    
+    for c in classes:
+        tp += np.sum((y_true==c) & (y_pred==c)) 
+        fp += np.sum((y_true!=c) & (y_pred==c)) 
+        fn += np.sum((y_true==c) & (y_pred!=c)) 
 
 
-    return ans if (2*TP + FP + FN) != 0 else 0.0
+    f1 = 2 * tp / (2*tp + fp + fn) if (2*tp + fp + fn)>0 else 0.0
+
+    return f1
