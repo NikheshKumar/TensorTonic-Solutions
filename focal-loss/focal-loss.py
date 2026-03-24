237f85b4214af9d1a1ad44e2eb08c7c45bd64a79
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-03-24 10:54:59 +0530

    Update solution for focal-loss

--- focal-loss/focal-loss.py
+++ focal-loss/focal-loss.py
@@ -5,12 +5,12 @@ def focal_loss(p, y, gamma=2.0):
     Compute Focal Loss for binary classification.
     """
     # Write code here
-    p = np.asarray(p)
-    y = np.asarray(y)
+    p = np.asarray(p, float)
+    y = np.asarray(y, float)
 
-    eps = 1e-7
+    eps = 1e-8
     p = np.clip(p, eps, 1-eps)
 
-    fl = - ( (1-p)**(gamma) )*y*np.log(p) - (p**gamma)*(1-y)*np.log(1-p)
+    fl = -((1-p)**gamma)*y*np.log(p) - (p**gamma)*(1-y)*np.log(1-p)
 
-    return np.mean(fl)
+    return float(np.mean(fl))
