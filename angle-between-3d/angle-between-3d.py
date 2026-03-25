9f48d865f195ffd37729df1bd113bfe560d5ce48
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-03-25 23:41:39 +0530

    Update solution for angle-between-3d

--- angle-between-3d/angle-between-3d.py
+++ angle-between-3d/angle-between-3d.py
@@ -8,14 +8,17 @@ def angle_between_3d(v, w):
     v = np.asarray(v, float)
     w = np.asarray(w, float)
 
+    eps = 1e-7
     norm_v = np.sqrt(np.sum(v**2))
     norm_w = np.sqrt(np.sum(w**2))
 
-    if np.abs(norm_v) < 1e-10 or np.abs(norm_w) < 1e-10:
+    if np.minimum(np.abs(norm_v), np.abs(norm_w)) < eps:
         return np.nan
 
-    u = np.dot(v,w) / (norm_v * norm_w)
-    u = np.clip(u, -1.0, 1.0)
+    angle = np.dot(v, w)/(norm_v * norm_w)
+    angle = np.clip(angle, -1.0, 1.0)
+    angle = np.arccos(angle)
 
-    return np.arccos(u)   
+    return angle
 
+    
