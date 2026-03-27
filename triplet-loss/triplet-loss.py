09c6fcef4c2e0b441c0acfda179f888d13617ec1
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-03-27 16:26:31 +0530

    Update solution for triplet-loss

--- triplet-loss/triplet-loss.py
+++ triplet-loss/triplet-loss.py
@@ -5,12 +5,13 @@ def triplet_loss(anchor, positive, negative, margin=1.0):
     Compute Triplet Loss for embedding ranking.
     """
     # Write code here
-    anchor = np.atleast_2d(anchor)
-    positive = np.atleast_2d(positive)
-    negative = np.atleast_2d(negative)
+    positive =  np.asarray(positive, float)
+    negative = np.asarray(negative, float)
+    anchor = np.asarray(anchor, float)
 
-    d_ap = np.sum((anchor-positive)**2, axis=1)
-    d_an = np.sum((anchor-negative)**2, axis=1)
+
+    d_ap = np.sum( (anchor - positive )**2, axis=-1 )
+    d_an = np.sum( (anchor - negative)**2, axis=-1 )
 
     L = np.maximum(0, d_ap - d_an + margin)
 
