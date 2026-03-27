f2e3554be6bf39492bb2d0521bb31be7129361c0
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-03-27 16:36:57 +0530

    Update solution for dice-loss

--- dice-loss/dice-loss.py
+++ dice-loss/dice-loss.py
@@ -5,14 +5,14 @@ def dice_loss(p, y, eps=1e-8):
     Compute Dice Loss for segmentation.
     """
     # Write code here
-    p = np.asarray(p)
-    y = np.asarray(y)
+    p = np.array(p, float).ravel()
+    y = np.array(y, float).ravel()
 
-    p = p.flatten()
-    y = y.flatten()
-
-    num = 2 * np.sum(p*y) + eps
-
+    num = 2*np.sum(p*y) + eps 
     den = np.sum(p) + np.sum(y) + eps
 
-    return 1 - (num / den)
+    score = num / den
+
+    loss = 1 - score
+
+    return float(loss)
