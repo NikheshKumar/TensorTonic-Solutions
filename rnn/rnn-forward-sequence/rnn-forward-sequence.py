06118b6fbe75ef2f9db1582affb333ff470970fa
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-03-29 00:59:34 +0530

    Update solution for rnn-forward-sequence

--- rnn/rnn-forward-sequence/rnn-forward-sequence.py
+++ rnn/rnn-forward-sequence/rnn-forward-sequence.py
@@ -6,17 +6,19 @@ def rnn_forward(X: np.ndarray, h_0: np.ndarray,
     Forward pass through entire sequence.
     """
     # YOUR CODE HERE
+    B,T,N = X.shape
+    h_prev = h_0
+    h_list = []
 
-    batch_size, T, input_dim = X.shape
-    hidden_dim = h_0.shape[1]
-    hidden_states = []
-    h = h_0
-  
     for t in range(T):
-      x_t = X[:, t, :]
-      h = np.tanh( x_t @ W_xh.T + h @ W_hh + b_h)
-      hidden_states.append(h)
+        x_t = X[:,t,:]
 
-    hidden_states = np.stack(hidden_states, axis=1)
+        h = np.tanh(h_prev@W_hh.T + x_t@W_xh.T + b_h)
+        
+        h_prev = h
 
-    return hidden_states, h
+        h_list.append(h_prev)
+
+    h_list = np.stack(h_list, axis=1)
+
+    return h_list, h_prev
