5331aa0691461f59a92f6b11f5a8142084c2622b
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-03-28 19:31:41 +0530

    Update solution for rnn-step-forward

--- rnn-step-forward/rnn-step-forward.py
+++ rnn-step-forward/rnn-step-forward.py
@@ -5,9 +5,14 @@ def rnn_step_forward(x_t, h_prev, Wx, Wh, b):
     Returns: h_t of shape (H,)
     """
     # Write code here
-    x_t = np.asarray(x_t)
-    h_prev = np.asarray(h_prev)
     
-    h_t = np.tanh((x_t @ Wx) + (h_prev @ Wh) + b)
-    h_t = np.asarray(h_t)
+    x_t = np.asarray(x_t, float)
+    h_prev = np.asarray(h_prev, float)
+    Wx = np.asarray(Wx, float)
+    Wh = np.asarray(Wh, float)
+    b = np.asarray(b, float)
+
+
+    h_t = np.tanh(x_t @ Wx + h_prev @ Wh + b )
+
     return h_t
