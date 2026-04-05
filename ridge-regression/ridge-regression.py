d73a09394ec8f55743c6bf43ad509686104b84a4
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-04-05 23:53:50 +0530

    Update solution for ridge-regression

--- ridge-regression/ridge-regression.py
+++ ridge-regression/ridge-regression.py
@@ -3,11 +3,18 @@ def ridge_regression(X, y, lam):
     Compute ridge regression weights using the closed-form solution.
     """
     # Write code here
-    import numpy as np
-    X = np.asarray(X)
-    y = np.asarray(y)
+    import numpy as np 
 
-    Z = lam*np.eye(len(X[1]), len(X[0]))
-    w = (np.linalg.inv(X.T @X + Z) )@X.T @y
+    X = np.asarray(X, float)
+    y = np.asarray(y, float)
 
-    return w.tolist()
+    m,n = X.shape
+
+    id = np.eye(n,n)
+
+    beta = np.linalg.inv(X.T@X + lam*id)@X.T@y
+    
+    return beta.tolist()
+
+
+    
