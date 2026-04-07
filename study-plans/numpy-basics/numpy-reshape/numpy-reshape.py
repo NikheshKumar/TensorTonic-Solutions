f42f0abf8317c81682fcba6ee78168723afdea81
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-04-07 11:21:30 +0530

    Add solution for numpy-reshape

---
+++ study-plans/numpy-basics/numpy-reshape/numpy-reshape.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def reshape_array(data, operation):
+    """
+    Returns: ndarray of float64 with shape determined by the operation
+    """
+    data = np.asarray(data, dtype=np.float64)
+    
+    if operation=="flatten":
+        return data.flatten()
+        
+    if operation=="transpose":
+        return data.T
+
+    if operation=="add_batch":
+        return data[np.newaxis, ...]
+        
