019d5951ad961baa7aaa22e884d123be54d9e5e9
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-04-09 04:16:10 +0530

    Add solution for la-vector-norms

---
+++ study-plans/math-linear-algebra/la-vector-norms/la-vector-norms.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def vector_norms(v):
+    """
+    Returns: float64 array of shape (3,) containing [L1, L2, L-inf] norms.
+    """
+    v = np.asarray(v, np.float64)
+
+    l1 = np.linalg.norm(v, ord=1)
+    l2 = np.linalg.norm(v, ord=2)
+    l_inf = np.max(np.abs(v))
+
+    return l1, l2, l_inf
