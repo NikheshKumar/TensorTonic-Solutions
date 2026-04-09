c899858069df7df0697aeaa1dbbcb6b92ba3ea58
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-04-10 05:04:35 +0530

    Add solution for la-matrix-determinant

---
+++ study-plans/math-linear-algebra/la-matrix-determinant/la-matrix-determinant.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def matrix_determinant(A):
+    """
+    Returns: float, the determinant of square matrix A.
+    """
+    A = np.asarray(A, np.float64)
+    return np.linalg.det(A)
