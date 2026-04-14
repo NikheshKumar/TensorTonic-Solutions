743e240c643a6ee3901834a31548b175bab72069
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-04-14 22:55:34 +0530

    Add solution for la-svd-components

---
+++ study-plans/math-linear-algebra/la-svd-components/la-svd-components.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def svd(A):
+    """
+    Returns: tuple (U, s, Vt) where A = U @ diag(s) @ Vt.
+    """
+    A = np.asarray(A, np.float64)
+
+    U,s,Vt = np.linalg.svd(A, full_matrices=False)
+
+    return (U,s,Vt)
