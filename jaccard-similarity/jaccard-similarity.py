ca4a1d8401326b0155876ce9c040050019533ea1
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-04-15 06:40:54 +0530

    Update solution for jaccard-similarity

--- jaccard-similarity/jaccard-similarity.py
+++ jaccard-similarity/jaccard-similarity.py
@@ -5,9 +5,6 @@ def jaccard_similarity(set_a, set_b):
     # Write code here
     import numpy as np 
 
-    seta = np.asarray(set_a)
-    setb = np.asarray(set_b)
-
-    j = len(np.intersect1d(seta, setb)) / len(np.union1d(seta, setb)) if len(np.union1d(seta, setb))!=0 else 0.0
+    j = len(np.intersect1d(set_a, set_b)) / len(np.union1d(set_a, set_b)) if len(np.union1d(set_a, set_b)) != 0 else 0.0
 
     return j
