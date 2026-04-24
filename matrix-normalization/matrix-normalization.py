ccc4805672ab6674bada014fd17aa4c1bff3832e
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-04-24 12:44:46 +0530

    Update solution for matrix-normalization

--- matrix-normalization/matrix-normalization.py
+++ matrix-normalization/matrix-normalization.py
@@ -5,35 +5,33 @@ def matrix_normalization(matrix, axis=None, norm_type='l2'):
     Normalize a 2D matrix along specified axis using specified norm.
     """
     # Write code here
-
-    matrix = np.asarray(matrix, dtype=float)
+    matrix = np.asarray(matrix, np.float64)
 
     if matrix.ndim != 2:
         return None
 
-    if axis is not None:
-        if not isinstance(axis, int):
-            return None
-        if axis >= matrix.ndim or axis < -matrix.ndim:
-            return None    
+    if not isinstance(axis, int):
+            if axis is not None:
+                return None
 
-    if norm_type == 'l2':
-        ans = np.sqrt( np.sum(matrix**2, axis=axis, keepdims=True) )
+    if axis is not None and (axis >= matrix.ndim or axis < -matrix.ndim):
+            return None   
+    
+    if norm_type=="l1":
+        m = np.linalg.norm(matrix, ord=1, axis=axis, keepdims=True)
         
-    elif norm_type == 'l1':
-        ans = np.sum(np.abs(matrix), axis=axis, keepdims=True)
+    elif norm_type=="l2":
+        m = np.linalg.norm(matrix, ord='fro' if axis is None else 2, axis=axis, keepdims=True)
         
-    elif norm_type == 'max':  
-        ans = np.max(np.abs(matrix), axis=axis, keepdims=True) 
+    elif norm_type=="max":
+        m = np.linalg.norm(matrix, ord=np.inf, axis=axis, keepdims=True)
 
     else:
         return None
- 
-
-    ans[ans == 0] = 1     
-        
-    normalized_matrix = matrix / ans
     
 
-    return np.atleast_1d(normalized_matrix)    
+    normalized_matrix = np.divide(matrix, m, out=np.zeros_like(matrix), where= m!=0)
 
+    return normalized_matrix
+
+    
