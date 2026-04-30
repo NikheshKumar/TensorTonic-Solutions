9b806b5ee21f5a66ad64472c80d5e93fb45730aa
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-04-30 15:04:14 +0530

    Update solution for global-avg-pooling

--- global-avg-pooling/global-avg-pooling.py
+++ global-avg-pooling/global-avg-pooling.py
@@ -6,13 +6,13 @@ def global_avg_pool(x):
     Supports (C,H,W) => (C,) and (N,C,H,W) => (N,C).
     """
     # Write code here
-
     x = np.asarray(x, np.float64)
 
-    if x.ndim!=3 and x.ndim!=4:
-        raise ValueError(f"Expected input with 3 or 4 dimensions, instead {x.ndim}")
+    if x.ndim not in [3,4]:
+        raise ValueError(f"Expected input with 3 or 4 dimensions, instead received {x.ndim}")
 
+    H,W = x.shape[-2], x.shape[-1]
 
-    gap = np.mean(x, axis=(-2,-1))
+    gap = np.sum(x, axis=(-2,-1)) / (H*W)
 
     return gap.astype(np.float64)
