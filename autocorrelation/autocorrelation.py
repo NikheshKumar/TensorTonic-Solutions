95f08a50db698a790dee8ff8029d020dae5add19
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-04-30 05:36:05 +0530

    Add solution for autocorrelation

---
+++ autocorrelation/autocorrelation.py
@@ -0,0 +1,30 @@
+def autocorrelation(series, max_lag):
+    """
+    Compute the autocorrelation of a time series for lags 0 to max_lag.
+    """
+    # Write code here
+    import numpy as np 
+
+    x = np.asarray(series, np.float64)
+    n = len(x)
+
+    if n == 0:
+        return []
+
+    if np.var(x) == 0:
+        res = [0.0] * (max_lag + 1)
+        res[0] = 1.0
+        return res
+
+    res = []
+
+    for i in range(max_lag+1):
+
+        x_mean = np.mean(x)
+        gamma_0 = np.var(x)
+
+        gamma_k = np.sum( (x[:n-i] - x_mean)*(x[i:] - x_mean) ) / n
+
+        res.append((gamma_k/gamma_0).astype(np.float64))
+
+    return res
