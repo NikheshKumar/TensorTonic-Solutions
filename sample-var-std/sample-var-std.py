9d1806584d5824b6ef11000eda33d575319ca998
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-05-14 13:55:34 +0530

    Update solution for sample-var-std

--- sample-var-std/sample-var-std.py
+++ sample-var-std/sample-var-std.py
@@ -5,12 +5,6 @@ def sample_var_std(x):
     Compute sample variance and standard deviation.
     """
     # Write code here
-    x = np.asarray(x, float)
+    x = np.asarray(x, np.float64)
 
-    m = np.mean(x)
-    n = len(x)
-
-    sam_var = ( np.sum((x-m)**2) ) / (n-1)
-    std_dev = np.sqrt(sam_var)
-
-    return sam_var, std_dev
+    return np.var(x,ddof=1).astype(np.float64), np.std(x, ddof=1).astype(np.float64)
