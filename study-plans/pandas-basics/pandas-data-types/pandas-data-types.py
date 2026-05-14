bc2dd8371dda1a79d91664b8849bf7cd5c8308d5
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-05-14 23:59:25 +0530

    Add solution for pandas-data-types

---
+++ study-plans/pandas-basics/pandas-data-types/pandas-data-types.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def data_types_overview(data):
+    """
+    Returns: dict with 'dtypes', 'type_counts', 'num_columns'
+    """
+
+    df = pd.DataFrame(data)
+
+    ans = {'dtypes':df.dtypes.astype(str).to_dict(), 'type_counts':df.dtypes.astype(str).value_counts().to_dict(), "num_columns":len(df.columns)}
+
+    return ans
