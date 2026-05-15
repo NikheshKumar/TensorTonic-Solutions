96a9b75b611a454c2b20b59ee94501370cdb2cf3
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-05-16 02:02:17 +0530

    Update solution for frequency-encoding

--- frequency-encoding/frequency-encoding.py
+++ frequency-encoding/frequency-encoding.py
@@ -7,12 +7,15 @@ def frequency_encoding(values):
     from collections import Counter
 
     values = np.asarray(values)
+    n = len(values)
+
+    if n == 0:
+        return []
+
     count = Counter(values)
-    ans = np.zeros(len(values))
+
+    ans = [count[v]/ n for v in values]
+
+    return ans
+
     
-
-    for i in range(len(values)):
-      prob = count[values[i]] / len(values)
-      ans[i] = prob
-
-    return ans.tolist()
