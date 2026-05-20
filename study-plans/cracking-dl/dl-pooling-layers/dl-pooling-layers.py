608960445647284b5091910e539398334254041c
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-05-20 20:24:14 +0530

    Update solution for dl-pooling-layers

--- study-plans/cracking-dl/dl-pooling-layers/dl-pooling-layers.py
+++ study-plans/cracking-dl/dl-pooling-layers/dl-pooling-layers.py
@@ -12,18 +12,20 @@ def pooling(input, pool_size, stride, pool_type):
     H_out = (H-pool_size)//(stride) + 1
     W_out = (W-pool_size)//(stride) + 1
 
+    C_stride, H_stride, W_stride = input.strides
+
+    new_strides = (C_stride, H_stride * stride, W_stride * stride, H_stride, W_stride )
+
     output = np.zeros((C, H_out, W_out), np.float64)
 
+
+    windows = np.lib.stride_tricks.as_strided(input, shape=(C, H_out, W_out, pool_size, pool_size), strides=new_strides, writeable=False)
     
-    for c in range(C):
-        for i in range(H_out):
-            for j in range(W_out):
-                if pool_type == "max":
-                    m = n = pool_size
-                    output[c,i,j] = np.max(input[c, i*stride:i*stride+m, j*stride:j*stride+n])
-                elif pool_type == "average":
-                    m = n = pool_size
-                    output[c,i,j] = np.mean(input[c, i*stride:i*stride+m, j*stride:j*stride+n])
+    if pool_type == "max":
+        output = np.max(windows, axis=(-2,-1))
+        
+    elif pool_type == "average":
+        output = np.mean(windows, axis=(-2,-1))
 
 
     return [[[round(float(output[c,i,j]), 4) for j in range(W_out)] for i in range(H_out)] for c in range(C)]
