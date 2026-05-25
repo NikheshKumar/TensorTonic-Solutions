bc3ecf789c5be88ed2e20144018cc475dc83b17b
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-05-26 05:24:42 +0530

    Update solution for majority-classifier

--- majority-classifier/majority-classifier.py
+++ majority-classifier/majority-classifier.py
@@ -5,15 +5,14 @@ def majority_classifier(y_train, X_test):
     Predict the most frequent label in training data for all test samples.
     """
     # Write code here
-    y_train = np.asarray(y_train)
+    y_train = np.asarray(y_train, dtype=int)
     X_test = np.asarray(X_test)
-  
-    cla, cla_freq = np.unique(y_train, return_counts=True)
 
-    val = cla[np.argmax(cla_freq)]
+    cla, counts = np.unique(y_train, return_counts=True)
 
-    res = np.full_like(X_test, val)
+    maj = cla[np.argmax(counts)]
 
-    return res
-  
+    y_pred = np.full_like(X_test, maj, dtype=int)
+
+    return y_pred
     
