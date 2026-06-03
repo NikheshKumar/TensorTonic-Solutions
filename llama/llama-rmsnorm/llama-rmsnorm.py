5e51f3bc370707a8223e8ff98fce1e37ef1197d4
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-06-03 15:50:18 +0530

    Add solution for llama-rmsnorm

---
+++ llama/llama-rmsnorm/llama-rmsnorm.py
@@ -0,0 +1,11 @@
+import torch
+
+def rms_norm(x: torch.Tensor, gamma: torch.Tensor, eps: float = 1e-6) -> torch.Tensor:
+    """
+    Returns: Normalized tensor of same shape as x
+    """
+    # YOUR CODE HERE
+    num = x * gamma
+    den = torch.sqrt(torch.mean(x**2, dim=-1, keepdim=True) + eps)
+
+    return num/den
