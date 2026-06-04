32b11c229a41194e112fcc663a992848e32169bf
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-06-04 07:41:00 +0530

    Update solution for swish-activation

--- swish-activation/swish-activation.py
+++ swish-activation/swish-activation.py
@@ -5,6 +5,9 @@ def swish(x):
     Implement Swish activation function.
     """
     # Write code here
+    x = np.atleast_1d(np.array(x, np.float64))
+    x = np.clip(x, -100, 100)
+    
     num = x * np.exp(x)
     den = 1 + np.exp(x)
 
