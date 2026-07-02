bed569766700b49771875c7e75daffe8ac712750
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-07-02 23:20:58 +0530

    Solve adagrad-optimizer

--- adagrad-optimizer/adagrad-optimizer.py
+++ adagrad-optimizer/adagrad-optimizer.py
@@ -5,12 +5,13 @@ def adagrad_step(w, g, G, lr=0.01, eps=1e-8):
     Perform one AdaGrad update step.
     """
     # Write code here
-    w = np.asarray(w, np.float64)
     g = np.asarray(g, np.float64)
     G = np.asarray(G, np.float64)
+    w = np.asarray(w, np.float64)
 
-    new_G = G + g**2
+    G = G + g**2
 
-    new_w = w - lr*g / np.sqrt(new_G + eps)
+    w = w - lr*g/(np.sqrt(G + eps))
 
-    return new_w, new_G
+
+    return w, G
