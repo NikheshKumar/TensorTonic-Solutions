e5ddad2ad0f768c8eb98c0cfaa4216c8ca9e1cdb
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-07-03 19:21:58 +0530

    Solve cosine-annealing-lr

--- cosine-annealing-lr/cosine-annealing-lr.py
+++ cosine-annealing-lr/cosine-annealing-lr.py
@@ -3,11 +3,12 @@ def cosine_annealing_schedule(base_lr, min_lr, total_steps, current_step):
     Compute the learning rate using cosine annealing.
     """
     # Write code here
-
     import numpy as np 
 
-    k = 1 + np.cos(np.pi*current_step / total_steps)
+    term = np.pi * current_step / total_steps
 
-    lr = min_lr + (base_lr - min_lr) * (k) / 2
+    lr = min_lr + 0.5*(base_lr - min_lr)*(1 + np.cos(term))
 
     return lr
+
+    
