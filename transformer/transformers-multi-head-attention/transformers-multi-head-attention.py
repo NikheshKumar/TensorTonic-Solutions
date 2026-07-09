6095069267d964d061d6816b828f8192f4ed31ff
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-07-09 12:51:56 +0530

    Solve transformers-multi-head-attention

--- transformer/transformers-multi-head-attention/transformers-multi-head-attention.py
+++ transformer/transformers-multi-head-attention/transformers-multi-head-attention.py
@@ -11,31 +11,27 @@ def multi_head_attention(Q: np.ndarray, K: np.ndarray, V: np.ndarray,
     Compute multi-head attention.
     """
     # Your code here
-    batch_size, seq_len, d_model = Q.shape
+    import numpy as np 
+    import math 
+    
+    batch_size, seq_length, d_model = Q.shape
     d_k = d_model // num_heads
 
-    q = Q @ W_q
-    k = K @ W_k
-    v = V @ W_v
+    Q_proj = (Q @ W_q).reshape(batch_size, seq_length, num_heads, d_k).transpose(0,2,1,3)
+    K_proj = (K @ W_k).reshape(batch_size, seq_length, num_heads, d_k).transpose(0,2,1,3)
+    V_proj = (V @ W_v).reshape(batch_size, seq_length, num_heads, d_k).transpose(0,2,1,3)
 
+    scores = Q_proj @ K_proj.transpose(0, 1, 3, 2)
+    scaled_scores = scores / math.sqrt(d_k)
 
-    q = (q.reshape(batch_size, seq_len, num_heads, d_k)).transpose(0, 2, 1, 3)
-    k = (k.reshape(batch_size, seq_len, num_heads, d_k)).transpose(0, 2, 1, 3)
-    v = (v.reshape(batch_size, seq_len, num_heads, d_k)).transpose(0, 2, 1, 3)
+    weights = softmax(scaled_scores, axis=-1)
+
+    context = (weights @ V_proj).transpose(0, 2, 1, 3).reshape(batch_size, seq_length, d_model)
+
+    mha = context @ W_o
+
+    return mha
+
     
-    # Scaled Dot product attentions
-  
-    scores = q @ k.transpose(0,1,3,2) / np.sqrt(d_k)
 
-    softmax_scores = softmax(scores, axis=1)
-
-
-    # Concatenation of multi head attention
-
-    c = softmax_scores @ v
-
-    res = c.transpose(0, 2, 1, 3).reshape(batch_size, seq_len, d_model) @ W_o
-
-    return res
-
-  
+ 
