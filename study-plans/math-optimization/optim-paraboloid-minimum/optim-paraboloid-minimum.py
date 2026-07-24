63f1a14143d94cf44c16dc96900bc0ea07628412
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-07-25 00:36:06 +0530

    Solve optim-paraboloid-minimum

--- study-plans/math-optimization/optim-paraboloid-minimum/optim-paraboloid-minimum.py
+++ study-plans/math-optimization/optim-paraboloid-minimum/optim-paraboloid-minimum.py
@@ -2,13 +2,9 @@ def paraboloid_minimum(a, b, c, d, e):
     """
     Returns: dict with 'x_star', 'y_star', 'f_min' (floats), each rounded to 6 decimals
     """
-    import numpy as np
+    
+    x_star = np.round(-c/(2.0*a),6)
+    y_star = np.round(-d/(2.0*b),6)
+    f_min = np.round(e - (c*c/(4.0*a)) - (d*d/(4.0*b)),6)
 
-    x_star = -c / (2*a)
-
-    y_star = -d / (2*b)
-
-    f_min = a*(x_star**2) + b*(y_star**2) + c*x_star + d*y_star + e
-
-
-    return {'x_star':round(float(x_star),6), 'y_star':round(float(y_star),6), 'f_min':round(float(f_min),6) }
+    return {"x_star":x_star, "y_star":y_star, "f_min":f_min}
