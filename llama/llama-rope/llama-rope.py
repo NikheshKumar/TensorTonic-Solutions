3e0b1aec3c515554c486a73d3b317b447639dc30
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-08-07 17:58:52 +0530

    Solve llama-rope

--- llama/llama-rope/llama-rope.py
+++ llama/llama-rope/llama-rope.py
@@ -5,17 +5,19 @@ def apply_rope(q, k, freqs_cos, freqs_sin):
     Returns: tuple of (q_rotated, k_rotated) same shapes as input
     """
     # YOUR CODE HERE
+    q_rotated = q.clone()
+    k_rotated = k.clone()
 
-    freqs_cos = freqs_cos.unsqueeze(0).unsqueeze(0)
-    freqs_sin = freqs_sin.unsqueeze(0).unsqueeze(0)
+    q_even = q[...,0::2]
+    q_odd = q[...,1::2]
 
-    q_rotated = torch.zeros_like(q)
-    q_rotated[..., 0::2] = q[..., 0::2] * freqs_cos - q[..., 1::2] * freqs_sin
-    q_rotated[..., 1::2] = q[..., 0::2] * freqs_sin + q[..., 1::2] * freqs_cos
-
-    k_rotated = torch.zeros_like(k)
-    k_rotated[..., 0::2] = k[..., 0::2] * freqs_cos - k[..., 1::2] * freqs_sin
-    k_rotated[..., 1::2] = k[..., 0::2] * freqs_sin + k[..., 1::2] * freqs_cos
+    k_even = k[...,0::2]
+    k_odd = k[...,1::2]
     
+    q_rotated[..., 0::2] = q_even * freqs_cos - q_odd * freqs_sin
+    q_rotated[..., 1::2] = q_even * freqs_sin + q_odd * freqs_cos
+
+    k_rotated[..., 0::2] = k_even * freqs_cos - k_odd * freqs_sin
+    k_rotated[..., 1::2] = k_even * freqs_sin + k_odd * freqs_cos
 
     return (q_rotated, k_rotated)
