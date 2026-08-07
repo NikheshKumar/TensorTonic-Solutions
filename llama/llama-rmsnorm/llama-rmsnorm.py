ba16eb113002a2acd93195100f2ed5baa51e9962
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-08-07 17:46:22 +0530

    Solve llama-rmsnorm

--- llama/llama-rmsnorm/llama-rmsnorm.py
+++ llama/llama-rmsnorm/llama-rmsnorm.py
@@ -8,4 +8,4 @@ def rms_norm(x: torch.Tensor, gamma: torch.Tensor, eps: float = 1e-6) -> torch.T
     num = x * gamma
     den = torch.sqrt(torch.mean(x**2, dim=-1, keepdim=True) + eps)
 
-    return num/den
+    return num / den
