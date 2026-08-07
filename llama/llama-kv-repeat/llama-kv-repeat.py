1d40c75be1498e53ac1e9d77ef3c845941a231be
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-08-07 18:41:47 +0530

    Solve llama-kv-repeat

--- llama/llama-kv-repeat/llama-kv-repeat.py
+++ llama/llama-kv-repeat/llama-kv-repeat.py
@@ -5,7 +5,11 @@ def repeat_kv(kv: torch.Tensor, n_rep: int) -> torch.Tensor:
     Returns: (batch, n_kv_heads * n_rep, seq_len, d_head)
     """
     # YOUR CODE HERE
+    if n_rep==1:
+        return kv
+        
+    batch, n_kv_heads, seq_len, d_head = kv.shape
     kv_head_rep = kv.repeat_interleave(n_rep, dim=1)
 
-    return kv_head_rep
+    return kv_head_rep.reshape(batch, n_kv_heads * n_rep, seq_len, d_head)
     
