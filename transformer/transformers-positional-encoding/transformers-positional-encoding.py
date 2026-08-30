d83a56761a01f247be637bc0bfd3e2d76277f050
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-08-30 16:42:17 +0530

    Solve transformers-positional-encoding

--- transformer/transformers-positional-encoding/transformers-positional-encoding.py
+++ transformer/transformers-positional-encoding/transformers-positional-encoding.py
@@ -5,15 +5,13 @@ def positional_encoding(seq_length: int, d_model: int) -> np.ndarray:
     Generate sinusoidal positional encodings.
     """
     # Your code here
-    
-    pe_matrix = np.zeros((seq_length, d_model), np.float64)
+    i = np.arange(0,d_model,2)
+    den = (1e4)**(i/d_model)
     pos = np.arange(seq_length).reshape(-1,1)
 
-    pe_matrix[:,0::2] = np.sin(pos * np.exp(np.arange(0, d_model, 2) * (-np.log(10000.0) / d_model)))
-    
-    pe_matrix[:,1::2] = np.cos(pos * np.exp(np.arange(0, d_model, 2) * (-np.log(10000.0) / d_model)))
+    pe = np.zeros((seq_length, d_model), dtype=np.float64)
 
+    pe[:,0::2] = np.sin(pos/den)
+    pe[:,1::2] = np.cos(pos/den)
 
-    return pe_matrix
-
-    
+    return pe
