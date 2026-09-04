c4b1de9080e72668f1dcd4b1ae0d6bb6ebd1e3fd
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-09-04 11:47:47 +0530

    Solve user-based-cf-prediction

--- user-based-cf-prediction/user-based-cf-prediction.py
+++ user-based-cf-prediction/user-based-cf-prediction.py
@@ -1,17 +1,15 @@
-def user_based_cf_prediction(similarities, ratings):
+def user_based_cf_prediction(similarities: list, ratings: list) -> float:
     """
-    Predict a rating using user-based collaborative filtering.
+    Returns the positive-similarity weighted rating prediction.
     """
     # Write code here
-    import numpy as np 
+    num = den = 0
+    
+    for s, r in zip(similarities, ratings):
+        if s>0:
+            num += s * r
+            den += s
 
-    similarities = np.asarray(similarities)
-    ratings = np.asarray(ratings)
 
-    mask = similarities > 0.0
-
-    num = np.sum(similarities[mask]*ratings[mask])
-
-    den = np.sum(similarities[mask])
-
-    return num/den if den>0.0 else 0.0
+    return num / den if den!=0 else 0
+        
