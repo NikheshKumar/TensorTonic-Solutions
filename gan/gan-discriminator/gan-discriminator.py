e39aa2f6904ad8a24131689a47b410219f586986
Nikhesh Kumar Murali <99962260+NikheshKumar@users.noreply.github.com>
2026-09-04 11:52:04 +0530

    Solve gan-discriminator

--- gan/gan-discriminator/gan-discriminator.py
+++ gan/gan-discriminator/gan-discriminator.py
@@ -1,33 +1,10 @@
 import numpy as np
 
-def discriminator(x: np.ndarray) -> np.ndarray:
+def discriminator(x: np.ndarray, W: np.ndarray) -> np.ndarray:
     """
-    Classify inputs as real or fake.
+    Returns discriminator probabilities as a float64 array with shape (B, 1).
     """
-    # Your implementation here
-    x = np.asarray(x, float)
-    eps = 1e-2
+    z = x@W
+    d = np.exp(z) / (1+np.exp(z))
 
-    def leaky_relu(x, alpha=0.2):
-      return np.maximum(alpha*x, x)
-
-    W1 = np.random.uniform(-eps, eps, (x.shape[1], x.shape[1]))
-    b1 = np.random.uniform(-eps, eps, (x.shape[1],))
-    h1 = leaky_relu(x @ W1 + b1)
-
-
-    W2 = np.random.uniform(-eps, eps, (h1.shape[1], h1.shape[1]))
-    b2 = np.random.uniform(-eps, eps, (h1.shape[1],))
-    h2 = leaky_relu(h1 @ W2 + b2)
-
-
-    W3 = np.random.uniform(-eps, eps, (h2.shape[1], 1))
-    b3 = np.random.uniform(-eps, eps, (1,))
-  
-    y = h2 @ W3 + b3
-
-    sig = 1 / (1 + np.exp(-np.clip(y, -500, 500)))
-
-    return sig
-
-    
+    return d
